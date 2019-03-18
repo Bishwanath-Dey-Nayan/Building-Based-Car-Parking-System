25eb7c31806ec8a6b052cbaf3400b2a028acc3cf
Bishwanath Dey Nayan <16103022@iubat.edu>
2019-03-18 23:36:37 +0600

    RegistrationVM Added

---
+++ CarParkingSystem/CarParkingSystem/Models/ViewModel/RegistrationVM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarParkingSystem.Models.ViewModel
+{
+    public class RegistrationVM
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Name is required.")]
+        [Display(Name="Client Name")]
+        public string Name { get; set; }
+
+        [Required]
+        
+        public string Contact { get; set; }
+
+        [Required]
+        [Display(Name="Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        [Display(Name ="Gender")]
+        public string Gender { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Address is Required")]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Email Address is Required.")]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name="Car Number.")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Car Number Required.")]
+        public string CarNo { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Car Name or Brand Name")]
+        [Display(Name="Car Name")]
+        public string CarName { get; set; }
+
+        [Display(Name="License Number.")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="License Number required.")]
+        public string LicenseNo { get; set; }
+        
+        
+    }
+}
