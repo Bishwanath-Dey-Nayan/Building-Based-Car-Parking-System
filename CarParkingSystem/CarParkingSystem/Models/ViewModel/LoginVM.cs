afc00eb83eeee1b61571f6a11147259add4eba2d
Bishwanath Dey Nayan <16103022@iubat.edu>
2019-03-20 00:21:59 +0600

    loginvmadded

---
+++ CarParkingSystem/CarParkingSystem/Models/ViewModel/LoginVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarParkingSystem.Models.ViewModel
+{
+    public class LoginVM
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Email Address is required.")]
+        [EmailAddress]
+        [Display(Name ="Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name ="Password")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please Enter the password.")]
+        public string Password { get; set; }
+
+
+    }
+}
