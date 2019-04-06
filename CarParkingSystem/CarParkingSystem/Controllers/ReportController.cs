0cbaf2e3cbe86d3f1d7b913eafe957fd25193ef3
Bishwanath Dey Nayan <16103022@iubat.edu>
2019-04-06 23:34:23 +0600

    Report view page modified

---
+++ CarParkingSystem/CarParkingSystem/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarParkingSystem.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ActionResult Report()
+        {
+            return View();
+        }
+        public ActionResult IncomeReport()
+        {
+            return View();
+        }
+    }
+}
