4bd8d1cd55096874836b0caca3057b6d081df1eb
Rakib Uddin <16103030@iubat.edu>
2019-04-07 21:18:38 +0600

    Everything ReDesigned

---
+++ CarParkingSystem/CarParkingSystem/Scripts/logScript.js
@@ -0,0 +1,28 @@
+﻿// Variables
+let ldate = new Date();
+let ltime = ldate.toLocaleTimeString();
+let store = ltime.replace(" ", ":")
+let timeArr = store.split(":")
+let greet = document.querySelector("#greet");
+let hour = parseInt(timeArr[0])
+let minuit = parseInt(timeArr[1])
+let AP = timeArr[3]
+
+
+// Conditional Rendaring
+if (hour > 6 && AP === "AM") {
+    console.log("Good Morning Admin")
+    greet.innerHTML = "Good Morning Admin"
+}
+if (hour === 12 && minuit === 0 && AP === "PM") {
+    console.log("Good Afternoon Admin")
+    greet.innerHTML = "Good Afternoon Admin"
+}
+if (hour <= 12 && AP === "PM") {
+    console.log("Good Evening Admin")
+    greet.innerHTML = "Good Evening Admin"
+}
+if (hour < 12 && AP === "AM") {
+    console.log("Good Night Admin")
+    greet.innerHTML = "Good Night Admin"
+}
---
+++ CarParkingSystem/CarParkingSystem/Scripts/scrypt.js
@@ -0,0 +1,14 @@
+﻿//Variables
+let welcome = document.querySelector(".welcome");
+let floor = document.querySelector(".shFloor");
+let time = document.querySelector(".time");
+
+// Time & Date
+const Localtime = () => {
+    let today = new Date();
+    let todayTime = today.toLocaleTimeString();
+    let todayDate = today.toLocaleDateString();
+    time.innerHTML = todayTime + " || " + todayDate
+}
+setInterval(Localtime , 1000)
+
